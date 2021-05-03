ce3caeebbe8775988c40b613cb835203260710d1
igorGabriel <igorgabriel0155@gmail.com>
2021-05-03 16:44:09 -0300

    Adicionando o oitavo exercicio

---
+++ Curso_Senac/Exercicios/At8_Pag_Imposto/js/main.js
@@ -0,0 +1,9 @@
+function tributaveis() {
+    //na hora que clicar aparece um alerta
+    alert(`Rendimentos tributáveis são todos os valores recebidos pelo contribuinte que podem sofrer a incidência da cobrança do Imposto de Renda, ou seja, eles entram no cálculo da declaração, como salários, férias e recebimento de aluguéis.`)
+    
+}
+
+function previdencia() {
+    alert(`Previdencia Oficial é a previdência de caráter obrigatório, instituída e administrada pelo Estado, compreendendo o Regime Próprio de Previdência Social – RPPS da União, dos Estados, do Distrito Federal e dos Municípios e o Regime Geral de Previdência Social – RGPS.`)
+}
