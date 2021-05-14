b5aa0230d8c5111e688fd5d369d90ff38ba8bdc5
Igor G. Rodrigues <igorgabriel0155@gmail.com>
2021-05-14 00:44:56 -0300

    at

---
+++ Curso_Senac/Exercicios/At13_Tabuada/js/main.js
@@ -0,0 +1,15 @@
+     let saida = document.getElementById('Saida')
+
+        for (let j = 1; j <= 10; j++) {
+
+            saida.innerHTML += `<br><h3 class="border-top border-left border-right mt-2 pt-3">Tabuada J do ${j}</h3><br>`
+
+            for (let i = 1; i <= 10; i++) {
+
+                let soma = j * i
+
+                saida.innerHTML += `<h4 class="border-left border-right ">${j} X ${i} = ${soma} </h4>`
+
+            }
+        }
+
